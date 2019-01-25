0442324fc9f4c41005e7326f0fc023c6498f0a19
p455w0rd <p455w0rd@gmail.com>
2019-01-25 18:45:10 -0500

    bump version

--- src/main/java/p455w0rd/orechidendium/init/ModGlobals.java
+++ src/main/java/p455w0rd/orechidendium/init/ModGlobals.java
@@ -8,7 +8,7 @@ public class ModGlobals {
 
 	public static final String MODID = "orechidendium";
 	public static final String NAME = "Orechid Endium";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	public static final String SERVER_PROXY = "p455w0rd.orechidendium.proxy.ProxyCommon";
 	public static final String CLIENT_PROXY = "p455w0rd.orechidendium.proxy.ProxyClient";
 	public static final String CONFIG_FILE = "config/OrechidEndium.cfg";
